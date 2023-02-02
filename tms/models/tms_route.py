9270b653621d929e638d55cd7f8d88287cbcb4fd
tap222 <mtapas.mohanty95@gmail.com>
2023-02-02 22:49:43 +0530

    Update tms_route.py

--- tms/models/tms_route.py
+++ tms/models/tms_route.py
@@ -87,13 +87,12 @@ class TmsRoute(models.Model):
             import sys
             origins = (str(departure['latitude']) + ',' + str(departure['longitude']))
             destinations = (str(arrival['latitude']) + ',' + str(arrival['longitude']))
-            key = self.env['ir.config_parameter'].get_param('mapquest.key')
             params = {
                 'origins': origins,
                 'destinations': destinations,
                 'mode': 'driving',
                 'language': self.env.lang,
-                'key': key,
+                'key': {key},
             }
             try:
                 result = json.loads(requests.get(url, params).content)
@@ -114,6 +113,7 @@ class TmsRoute(models.Model):
                 self.travel_time = duration
             except Exception:
                 raise exceptions.UserError(_("Google Maps is not available."))
+
 
     def open_in_google(self):
         for route in self:
